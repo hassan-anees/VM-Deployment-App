375684731b8735f8090034904088ca9ea63b894b
Hassan Anees <hanees@uoguelph.ca>
2020-02-24 15:10:27 -0500

    testing azure bash to create with ubuntu

--- a2azure.sh
+++ a2azure.sh
@@ -2,16 +2,11 @@
 OLDIFS=$IFS
 IFS=","
 
-#debian
 debian="Debian:debian-10-daily:10-gen2:0.20200222.178"
-
-#redhat
 redhat="RHEL"
-
-#ubuntu
 ubuntu="cognosys:ubuntu-1604-lts:ubuntu-16-04-lts:1.2019.0711"
 
-create_vm() {
+createVm() {
 
     #first need create resource
     az group create --name myResourceGroup2 --location eastus
@@ -27,26 +22,25 @@ create_vm() {
     #going to read the docker file. so have a while loop 
     #in that while loop when you read it, create 
     # #here get the ip 
-    PUBIP=$(az vm show -d -g myResourceGroup2 -n $1 --query publicIps -o tsv)
-    echo "$PUBIP is the ip"
+    IPADDR=$(az vm show -d -g myResourceGroup2 -n $1 --query publicIps -o tsv)
+    echo "$IPADDR is the ip"
 
     #reading the docker
     while read name dockerImg registry background
         do
             if [ "$2" == "$redhat" ]; then
-                ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo yum install -y docker device-mapper-libs device-mapper-event-libs
-                ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo systemctl enable --now docker.service
+                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo yum install -y docker device-mapper-libs device-mapper-event-libs
+                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo systemctl enable --now docker.service
             else 
                 if [ "$2" == "$ubuntu" ]; then
                     sleep 600
                 fi
-                ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo apt -y install docker.io
-                ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo systemctl start docker     
+                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo apt -y install docker.io
+                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo systemctl start docker     
             fi
-            ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo docker pull "$dockerImg"
-            ssh -o "StrictHostKeyChecking no" -n $PUBIP sudo docker run --rm "$dockerImg"
+            ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo docker pull "$dockerImg"
+            ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo docker run --rm "$dockerImg"
 
-            echo "$1 $2 $3 $4"
         done < $"dockerFileAzure.csv"
 
 }
@@ -56,10 +50,10 @@ create_vm() {
 # }
 
 #CHANGE THING LATER
-while read platform name os size key
+while read platform name imageId imageSize key
     do
         if [ "$platform" == "Azure" ]; then
-            create_vm "$name" "$os" "$size" "$key"
+            createVm "$name" "$imageId" "$imageSize" "$key"
         fi
     done < $"vmConfigAzure.csv"
 
