f612286581e48d86c2d2651eecef1d0e7567bc0a
Hassan Anees <hanees@uoguelph.ca>
2020-02-25 17:26:48 -0500

    Monitoring finished. Test cases finished (2 azure, 2 aws)

--- a2azure.sh
+++ a2azure.sh
@@ -4,7 +4,23 @@ IFS=","
 
 debian="Debian:debian-10-daily:10-gen2:0.20200222.178"
 redhat="RHEL"
-ubuntu="cognosys:ubuntu-1604-lts:ubuntu-16-04-lts:1.2019.0711"
+#ubuntu="cognosys:ubuntu-1604-lts:ubuntu-16-04-lts:1.2019.0711"
+ubuntu="Canonical:UbuntuServer:16.04-LTS:latest"
+
+docker1(){
+    echo "$1 is the ip"
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo apt-get update
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo apt -y install docker.io
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo systemctl start docker
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo docker run --rm $2
+}
+
+docker0(){
+
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo yum install -y docker device-mapper-libs device-mapper-event-libs
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo systemctl enable --now docker.service
+    ssh -o "StrictHostKeyChecking no" -n $1 sudo docker run --rm $2
+}
 
 createVm() {
 
@@ -24,23 +40,28 @@ createVm() {
     # #here get the ip 
     IPADDR=$(az vm show -d -g myResourceGroup2 -n $1 --query publicIps -o tsv)
     echo "$IPADDR is the ip"
-
+    
+    echo "name($1) imageId($2) size($3) key($4) "
     #reading the docker
     while read name dockerImg registry background
         do
+            # echo "name($name) dockImg($dockerImg) regis($registry) bac($background) "
             if [ "$2" == "$redhat" ]; then
-                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo yum install -y docker device-mapper-libs device-mapper-event-libs
-                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo systemctl enable --now docker.service
-            else 
-                if [ "$2" == "$ubuntu" ]; then
-                    sleep 600
+                if [ "$1" == "$name" ]; then
+                    echo "FOUND SAME REDHAT"
+                    echo "$name $dockerImg $registry $background"
+                    docker0 "$IPADDR" "$dockerImg"
                 fi
-                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo apt -y install docker.io
-                ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo systemctl start docker     
+            else 
+                if [ "$2" == "$ubuntu" ] || [ "$2" == "$debian" ]; then
+                    sleep 1
+                    if [ "$1" == "$name" ]; then
+                        echo "FOUND FROM DOCKER"
+                        echo "$name $dockerImg $registry $background"
+                        docker1 "$IPADDR" "$dockerImg"
+                    fi     
+                fi
             fi
-            ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo docker pull "$dockerImg"
-            ssh -o "StrictHostKeyChecking no" -n $IPADDR sudo docker run --rm "$dockerImg"
-
         done < $"dockerFileAzure.csv"
 
 }
--- a2azureMonitor.sh
+++ a2azureMonitor.sh
@@ -1,32 +1,79 @@
 #!/bin/bash
-OLDIFS=$IFS
-IFS=","
+
 
 machines=$(az vm list --query "[].osProfile.computerName" -o tsv)
 
 for machine in $machines; do
     options=$(az vm show -d -g myResourceGroup2 -n $machine --query "[name, powerState, publicIps, storageProfile.imageReference.offer, location]" -o tsv)
     IFS=$'\n' arr=( $options )
-   
+    # echo "VM Name = ${arr[0]}"
+    # echo "Current Machine State = ${arr[1]}"
+    # echo "Public IP Address = ${arr[2]}"
+    # echo "OS Name = ${arr[3]}"
+    # echo "Region = ${arr[4]}"
+    echo ""
     IFS=","
 
-    #reading the docker
-    while read name dockerImg registry background
-        do
-          # echo "($arr)"
-          # echo "$name"
-          # echo "${arr[0]}"
-          if [ "${arr[0]}" == "$name" ]; then
-            echo "The IP is ${arr[2]}"
-            echo "Docker Current downloaded images"
-            ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker images
-            echo "Docker Running images"
-            ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker ps
-          fi
-        done < $"dockerFileAzure.csv"
+    # while read name dockerImg registry background
+    #     do
+    #       # echo "($arr)"
+    #       # echo "$name"
+    #       # echo "${arr[0]}"
+          # if [ "${arr[0]}" == "$name" ]; then
+    echo "The IP is ${arr[2]}"
+    echo "Docker Current downloaded images"
+    ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker images
+    echo "Docker Running images"
+    ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker ps
+          # fi
+        # done < $"dockerFileAzure.csv"
 
+    # ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker images
+    # echo ""
+    # ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker ps
 
+    # sleep 5
+
+  echo ""
   
 done
 
-IFS=$OLDIFS
+
+# OLDIFS=$IFS
+# IFS=","
+
+# echo "LOL"
+# machines=$(az vm list --query "[].osProfile.computerName" -o tsv)
+# echo "HA"
+# echo $machines
+# echo "stuff"
+
+# for machine in $machines; do
+#     echo "$yo"
+    
+#     options=$(az vm show -d -g myResourceGroup2 -n $machine --query "[name, powerState, publicIps, storageProfile.imageReference.offer, location]" -o tsv)
+#     IFS=$'\n' arr=( $options )
+#     echo "$yo"
+   
+#     IFS=","
+
+#     #reading the docker
+#     while read name dockerImg registry background
+#         do
+#           # echo "($arr)"
+#           # echo "$name"
+#           # echo "${arr[0]}"
+#           if [ "${arr[0]}" == "$name" ]; then
+#             echo "The IP is ${arr[2]}"
+#             echo "Docker Current downloaded images"
+#             ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker images
+#             echo "Docker Running images"
+#             ssh -o "StrictHostKeyChecking no" -n ${arr[2]} sudo docker ps
+#           fi
+#         done < $"dockerFileAzure.csv"
+
+
+  
+# done
+
+# IFS=$OLDIFS
